5baeb1b51be60a512dd81a8fd3749fbd7b19ec62
Takács Bendegúz <t.bebe99@gmail.com>
2023-12-03 12:25:30 +0100

    Day 2 Completed!

--- AdventOfCode2023.cpp
+++ AdventOfCode2023.cpp
@@ -7,7 +7,7 @@
 int main()
 {
     //cout << day_one::findExtendedSumOfNumbers("inputFiles/day01_01.txt") << endl;
-    cout << day_two::sumOfGameIDS("inputFiles/day02_01.txt")<<endl;
+    cout << day_two::sumOfPowers("inputFiles/day02_01.txt")<<endl;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
--- dayHeaders/day_two.h
+++ dayHeaders/day_two.h
@@ -34,4 +34,31 @@ namespace day_two {
 		}
 		return sum;
 	}
+	unsigned int sumOfPowers(const string& fileName) {
+		ifstream fileStream(fileName);
+		string line;
+		unsigned int sum = 0;
+		map<string, int> ballCounter({ {"blue",0},{"red",0},{"green",0} });
+		while (getline(fileStream, line)) {
+			regex splitter(R"(:|;)");
+			vector<string> tokens(sregex_token_iterator{ begin(line),end(line),splitter,-1 }, sregex_token_iterator{});
+			tokens.erase(tokens.begin());
+			regex commaSplit(R"(,)");
+			for (string balls : tokens) {
+				vector<string> colorstr(sregex_token_iterator{ begin(balls),end(balls),commaSplit,-1 }, sregex_token_iterator{});
+				for (string ball : colorstr) {
+					stringstream ss(ball);
+					string color;
+					int number;
+					ss >> number >> color;
+					ballCounter[color] = ballCounter[color] > number ? ballCounter[color] : number;
+				}
+			}
+			sum += ballCounter["red"] * ballCounter["green"] * ballCounter["blue"];
+			ballCounter["red"] = 0;
+			ballCounter["blue"] = 0;
+			ballCounter["green"] = 0;
+		}
+		return sum;
+	}
 }
